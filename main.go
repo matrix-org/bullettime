e6e44fc9acf0c620bce13e8b173296319b6aa106
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2015-07-30 18:03:49 +0200

    added hello world server
    Signed-off-by: Patrik Oldsberg <patrik.oldsberg@ericsson.com>

--- main.go
+++ main.go
@@ -1,21 +1,32 @@
-// Copyright 2015 OpenMarket Ltd
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+type testHandler struct{}
+
+func (t testHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	rw.Write([]byte("Hello World"))
+}
 
 func main() {
-    fmt.Println("Welcome to ganglion. All your synapse are belong to us.")
+	handler := testHandler{}
+	mux := http.NewServeMux()
+	mux.Handle("/", handler)
+
+	port := "4080"
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+	}
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Println("Listening on port " + port)
+	log.Fatal(server.ListenAndServe())
 }
