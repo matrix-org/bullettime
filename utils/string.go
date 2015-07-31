d2738c86ca40fcb18caecb7db61a03ac145f16a2
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2015-07-31 21:00:29 +0200

    added utils package, with string utils
    Signed-off-by: Patrik Oldsberg <patrik.oldsberg@ericsson.com>

---
+++ utils/string.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+const randomStringCharset = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789"
+
+var randomStringCharsetLength = big.NewInt(int64(len(randomStringCharset)))
+
+func RandomString(length int) string {
+	result := make([]byte, length)
+	for i := 0; i < length; i++ {
+		value, err := rand.Int(rand.Reader, randomStringCharsetLength)
+		if err != nil {
+			panic(err)
+		}
+		result[i] = randomStringCharset[value.Int64()]
+	}
+	return string(result)
+}
