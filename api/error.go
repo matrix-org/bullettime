beca570a3ee27265d23bfcdc3685826c98aa9d63
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2015-07-31 15:57:39 +0200

    added api/error with some predefined error responses
    Signed-off-by: Patrik Oldsberg <patrik.oldsberg@ericsson.com>

---
+++ api/error.go
@@ -0,0 +1,83 @@
+package api
+
+type Error struct {
+	Code    string `json:"errcode"`
+	Message string `json:"error"`
+	status  int
+}
+
+func (e *Error) Status() int {
+	return e.status
+}
+
+func UnrecognizedError(message string) *Error {
+	return &Error{
+		Code:    "M_UNRECOGNIZED",
+		Message: message,
+		status:  404,
+	}
+}
+
+func NotFoundError(message string) *Error {
+	return &Error{
+		Code:    "NOT_FOUND",
+		Message: message,
+		status:  404,
+	}
+}
+
+func UserInUseError(message string) *Error {
+	return &Error{
+		Code:    "M_USER_IN_USE",
+		Message: message,
+		status:  400,
+	}
+}
+
+func ForbiddenError(message string) *Error {
+	return &Error{
+		Code:    "M_FORBIDDEN",
+		Message: message,
+		status:  403,
+	}
+}
+
+func UnkownTokenError(message string) *Error {
+	return &Error{
+		Code:    "M_UNKNOWN_TOKEN",
+		Message: message,
+		status:  403,
+	}
+}
+
+func BadJsonError(message string) *Error {
+	return &Error{
+		Code:    "M_BAD_JSON",
+		Message: message,
+		status:  400,
+	}
+}
+
+func MissingTokenError(message string) *Error {
+	return &Error{
+		Code:    "M_MISSING_TOKEN",
+		Message: message,
+		status:  403,
+	}
+}
+
+func UnkownError(message string) *Error {
+	return &Error{
+		Code:    "M_UNKNOWN",
+		Message: message,
+		status:  400,
+	}
+}
+
+func ServerError(message string) *Error {
+	return &Error{
+		Code:    "M_SERVER_ERROR",
+		Message: message,
+		status:  500,
+	}
+}
