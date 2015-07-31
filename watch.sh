123c6ddd4d54e3d7724296c972cc7be1578b158d
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2015-07-31 23:10:56 +0200

    watch: use go 1.5 vendor experiment
    Signed-off-by: Patrik Oldsberg <patrik.oldsberg@ericsson.com>

--- watch.sh
+++ watch.sh
@@ -2,5 +2,5 @@
 
 browser-sync start --config bs-config.js > /dev/null 2>&1 &
 
-autoexec -sve "killall bullettime ; go build github.com/Rugvip/bullettime && ./bullettime 4080 & browser-sync reload --port 3080" main.go */*.go */*/*.go */*/*/*.go */*/*/*/*.go
+autoexec -sve "killall bullettime ; GO15VENDOREXPERIMENT=1 go build github.com/Rugvip/bullettime && ./bullettime 4080 & browser-sync reload --port 3080" main.go */*.go */*/*.go */*/*/*.go */*/*/*/*.go
 
