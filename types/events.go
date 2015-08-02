3693c43d842d559041f06484400f93fea5097244
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2015-08-02 18:59:44 +0200

    types/events: added typed content types
    Signed-off-by: Patrik Oldsberg <patrik.oldsberg@ericsson.com>

--- types/events.go
+++ types/events.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"encoding/json"
 	"strconv"
 	"time"
 )
@@ -11,11 +12,15 @@ const (
 	EventTypeCreate      EventType = "m.room.create"
 	EventTypeName                  = "m.room.name"
 	EventTypeTopic                 = "m.room.topic"
-	EventTypeMember                = "m.room.member"
 	EventTypeAliases               = "m.room.aliases"
 	EventTypeJoinRules             = "m.room.join_rules"
+	EventTypeMembership            = "m.room.member"
 	EventTypePowerLevels           = "m.room.power_levels"
 )
+
+type TypedContent interface {
+	EventType() EventType
+}
 
 type Content interface{}
 
@@ -26,6 +31,7 @@ type TimeStamp struct {
 type Event struct {
 	Id        EventId   `json:"event_id"`
 	RoomId    RoomId    `json:"room_id"`
+	UserId    UserId    `json:"user_od"`
 	EventType EventType `json:"type"`
 	TimeStamp TimeStamp `json:"origin_server_ts"`
 	Content   Content   `json:"content"`
@@ -52,12 +58,84 @@ func (ts *TimeStamp) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+type GenericContent struct {
+	Content   map[string]interface{}
+	eventType EventType
+}
+
+func (c *GenericContent) EventType() EventType {
+	return c.eventType
+}
+
+func (c *GenericContent) MarshalJSON() ([]byte, error) {
+	return json.Marshal(c.Content)
+}
+
 type TestContent struct {
 	Name string `json:"name"`
 }
 
-type MembershipContent struct {
+type MembershipEventContent struct {
 	Membership  string `json:"membership"`
 	DisplayName string `json:"displayname"`
 	AvatarUrl   string `json:"avatar_url"`
+}
+
+func (c *MembershipEventContent) EventType() EventType {
+	return EventTypeMembership
+}
+
+type CreateEventContent struct {
+	Creator UserId `json:"creator"`
+}
+
+func (c *CreateEventContent) EventType() EventType {
+	return EventTypeCreate
+}
+
+type NameEventContent struct {
+	Name string `json:"name"`
+}
+
+func (c *NameEventContent) EventType() EventType {
+	return EventTypeName
+}
+
+type TopicEventContent struct {
+	Topic string `json:"topic"`
+}
+
+func (c *TopicEventContent) EventType() EventType {
+	return EventTypeTopic
+}
+
+type AliasesEventContent struct {
+	Aliases string `json:"aliases"`
+}
+
+func (c *AliasesEventContent) EventType() EventType {
+	return EventTypeAliases
+}
+
+type PowerLevelsEventContent struct {
+	Ban          int             `json:"ban"`
+	Kick         int             `json:"kick"`
+	Redact       int             `json:"redact"`
+	UserDefault  int             `json:"users_default"`
+	CreateState  int             `json:"state_default"`
+	EventDefault int             `json:"events_default"`
+	Users        map[UserId]int  `json:"users"`
+	Events       map[EventId]int `json:"events"`
+}
+
+func (c *PowerLevelsEventContent) EventType() EventType {
+	return EventTypePowerLevels
+}
+
+type JoinRulesEventContent struct {
+	JoinRule JoinRule `json:"join_rule"`
+}
+
+func (c *JoinRulesEventContent) EventType() EventType {
+	return EventTypeJoinRules
 }
