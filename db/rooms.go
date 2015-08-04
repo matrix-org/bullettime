d83cce29c38aa1c77bcc8c0824ce85ce1d814a25
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2015-08-04 19:54:16 +0200

    db/rooms: made goroutine-safe
    Signed-off-by: Patrik Oldsberg <patrik.oldsberg@ericsson.com>

--- db/rooms.go
+++ db/rooms.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"sync"
 	"time"
 
 	"github.com/Rugvip/bullettime/interfaces"
@@ -8,10 +9,13 @@ import (
 	"github.com/Rugvip/bullettime/utils"
 )
 
-type roomDb struct {
-	events  map[types.EventId]types.Event
-	rooms   map[types.RoomId]*dbRoom
-	aliases map[types.Alias]*dbRoom
+type roomDb struct { // always lock in the same order as below
+	aliasesLock sync.RWMutex
+	aliases     map[types.Alias]*dbRoom
+	roomsLock   sync.RWMutex
+	rooms       map[types.RoomId]*dbRoom
+	eventsLock  sync.RWMutex
+	events      map[types.EventId]types.Event
 }
 
 func NewRoomDb() (interfaces.RoomStore, types.Error) {
@@ -27,18 +31,26 @@ type stateId struct {
 	StateKey  string
 }
 
-type dbRoom struct {
-	id     types.RoomId
-	states map[stateId]*types.State
-	events []types.Event
+type dbRoom struct { // always lock in the same order as below
+	id         types.RoomId
+	stateLock  sync.RWMutex
+	states     map[stateId]*types.State
+	eventsLock sync.RWMutex
+	events     []types.Event
 }
 
 func (db *roomDb) CreateRoom(hostname string, alias *types.Alias) (id types.RoomId, err types.Error) {
-	if alias != nil && db.aliases[*alias] != nil {
-		err = types.RoomInUseError("room alias '" + alias.String() + "' already exists")
-		return
+	if alias != nil {
+		db.aliasesLock.Lock()
+		defer db.aliasesLock.Unlock()
+		if db.aliases[*alias] != nil {
+			err = types.RoomInUseError("room alias '" + alias.String() + "' already exists")
+			return
+		}
 	}
 	id.Domain = hostname
+	db.roomsLock.Lock()
+	defer db.roomsLock.Unlock()
 	for {
 		id.Id.Id = utils.RandomString(16)
 		if db.rooms[id] == nil {
@@ -56,6 +68,8 @@ func (db *roomDb) CreateRoom(hostname string, alias *types.Alias) (id types.Room
 }
 
 func (db *roomDb) RoomExists(id types.RoomId) types.Error {
+	db.roomsLock.RLock()
+	defer db.roomsLock.RUnlock()
 	if db.rooms[id] == nil {
 		return types.NotFoundError("room '" + id.String() + "' doesn't exist")
 	}
@@ -63,10 +77,14 @@ func (db *roomDb) RoomExists(id types.RoomId) types.Error {
 }
 
 func (db *roomDb) AddRoomMessage(roomId types.RoomId, userId types.UserId, content types.TypedContent) (*types.Message, types.Error) {
+	db.roomsLock.RLock()
+	defer db.roomsLock.RUnlock()
 	room := db.rooms[roomId]
 	if room == nil {
 		return nil, types.NotFoundError("room '" + roomId.String() + "' doesn't exist")
 	}
+	db.eventsLock.Lock()
+	defer db.eventsLock.Unlock()
 	var eventId = types.EventId{types.Id{Domain: userId.Domain}}
 	for {
 		eventId.Id.Id = utils.RandomString(16)
@@ -83,16 +101,22 @@ func (db *roomDb) AddRoomMessage(roomId types.RoomId, userId types.UserId, conte
 	event.Content = content
 
 	db.events[eventId] = event
+	room.eventsLock.Lock()
+	defer room.eventsLock.Unlock()
 	room.events = append(room.events, event)
 
 	return event, nil
 }
 
 func (db *roomDb) SetRoomState(roomId types.RoomId, userId types.UserId, content types.TypedContent, stateKey string) (*types.State, types.Error) {
+	db.roomsLock.RLock()
+	defer db.roomsLock.RUnlock()
 	room := db.rooms[roomId]
 	if room == nil {
 		return nil, types.NotFoundError("room '" + roomId.String() + "' doesn't exist")
 	}
+	db.eventsLock.Lock()
+	defer db.eventsLock.Unlock()
 	var eventId = types.EventId{types.Id{Domain: userId.Domain}}
 	for {
 		eventId.Id.Id = utils.RandomString(16)
@@ -113,17 +137,25 @@ func (db *roomDb) SetRoomState(roomId types.RoomId, userId types.UserId, content
 	state.OldState = (*types.OldState)(room.states[stateId])
 
 	db.events[eventId] = state
+	room.eventsLock.Lock()
+	defer room.eventsLock.Unlock()
 	room.events = append(room.events, state)
+	room.stateLock.Lock()
+	defer room.stateLock.Unlock()
 	room.states[stateId] = state
 
 	return state, nil
 }
 
 func (db *roomDb) RoomState(roomId types.RoomId, eventType, stateKey string) (*types.State, types.Error) {
+	db.roomsLock.RLock()
+	defer db.roomsLock.RUnlock()
 	room := db.rooms[roomId]
 	if room == nil {
 		return nil, types.NotFoundError("room '" + roomId.String() + "' doesn't exist")
 	}
+	room.stateLock.RLock()
+	defer room.stateLock.RUnlock()
 	state := room.states[stateId{eventType, stateKey}]
 	return state, nil
 }
