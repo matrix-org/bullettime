9b4e400b137030cf5750fe922877234fc773c92b
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2015-08-05 17:23:12 +0200

    db: added membership db
    Signed-off-by: Patrik Oldsberg <patrik.oldsberg@ericsson.com>

---
+++ db/members.go
@@ -0,0 +1,85 @@
+package db
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/Rugvip/bullettime/interfaces"
+	"github.com/Rugvip/bullettime/types"
+)
+
+type memberDb struct { // always lock in the same order as below
+	sync.RWMutex
+	users   map[types.RoomId][]types.UserId
+	rooms   map[types.UserId][]types.RoomId
+	members map[memberKey]struct{}
+}
+
+type memberKey struct {
+	types.RoomId
+	types.UserId
+}
+
+func NewMembershipDb() (interfaces.MembershipStore, types.Error) {
+	return &memberDb{
+		users:   map[types.RoomId][]types.UserId{},
+		rooms:   map[types.UserId][]types.RoomId{},
+		members: map[memberKey]struct{}{},
+	}, nil
+}
+
+func (db *memberDb) AddMember(roomId types.RoomId, userId types.UserId) types.Error {
+	db.Lock()
+	defer db.Unlock()
+	member := memberKey{roomId, userId}
+	if _, ok := db.members[member]; ok {
+		msg := fmt.Sprintf("user %s is already a member of the room %s", userId, roomId)
+		return types.ServerError(msg)
+	}
+	db.members[member] = struct{}{}
+	db.users[roomId] = append(db.users[roomId], userId)
+	db.rooms[userId] = append(db.rooms[userId], roomId)
+	return nil
+}
+
+func (db *memberDb) RemoveMember(roomId types.RoomId, userId types.UserId) types.Error {
+	db.Lock()
+	defer db.Unlock()
+	member := memberKey{roomId, userId}
+	if _, ok := db.members[member]; !ok {
+		msg := fmt.Sprintf("user %s is not a member of the room %s", userId, roomId)
+		return types.ServerError(msg)
+	}
+	users := db.users[roomId]
+	for i, l := 0, len(users); i < l; i += 1 {
+		if users[i] == userId {
+			users[i] = users[l-1]
+			users[l-1] = types.UserId{}
+			users = users[:l-1]
+			break
+		}
+	}
+	rooms := db.rooms[userId]
+	for i, l := 0, len(rooms); i < l; i += 1 {
+		if rooms[i] == roomId {
+			rooms[i] = rooms[l-1]
+			rooms[l-1] = types.RoomId{}
+			rooms = rooms[:l-1]
+			break
+		}
+	}
+	delete(db.members, member)
+	return nil
+}
+
+func (db *memberDb) Rooms(userId types.UserId) ([]types.RoomId, types.Error) {
+	db.RLock()
+	defer db.RUnlock()
+	return db.rooms[userId], nil
+}
+
+func (db *memberDb) Users(roomId types.RoomId) ([]types.UserId, types.Error) {
+	db.RLock()
+	defer db.RUnlock()
+	return db.users[roomId], nil
+}
