bb3d0bc13ad919d50b7c03e2210e6ca052315b74
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2015-08-05 14:58:05 +0200

    types/stream: added StreamToken type
    Signed-off-by: Patrik Oldsberg <patrik.oldsberg@ericsson.com>

--- types/stream.go
+++ types/stream.go
@@ -1,5 +1,11 @@
 package types
 
+import (
+	"fmt"
+
+	"github.com/Rugvip/bullettime/utils"
+)
+
 type Pagination struct {
 	Start string        `json:"start"`
 	End   string        `json:"end"`
@@ -22,4 +28,41 @@ type RoomSummary struct {
 type RoomInitialSync struct {
 	RoomSummary
 	Presence PresenceEvent `json:"presence"`
+}
+
+type StreamToken struct {
+	MessageIndex  int64
+	PresenceIndex int64
+	TypingIndex   int64
+}
+
+type TokenParseError string
+
+func (e TokenParseError) Error() string {
+	return "failed to parse token: " + string(e)
+}
+
+func (t StreamToken) String() string {
+	return fmt.Sprintf("s%d_%d_%d", t.MessageIndex, t.PresenceIndex, t.TypingIndex)
+}
+
+func ParseStreamToken(str string) (StreamToken, error) {
+	var message, presence, typing int64
+	count, err := fmt.Sscanf(str, "s%d_%d_%d", &message, &presence, &typing)
+	if err != nil {
+		return StreamToken{}, TokenParseError(err.Error())
+	}
+	if count != 3 {
+		return StreamToken{}, TokenParseError("token does not match format")
+	}
+	return StreamToken{message, presence, typing}, nil
+}
+
+func (t *StreamToken) UnmarshalJSON(bytes []byte) (err error) {
+	*t, err = ParseStreamToken(utils.StripQuotes(string(bytes)))
+	return
+}
+
+func (t StreamToken) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + t.String() + `"`), nil
 }
