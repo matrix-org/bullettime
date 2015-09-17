2e2c6ed1d3e87db0e4f7f4487af19f8c27e792e5
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2015-09-17 23:25:47 +0200

    added test folder with service_test, intended for some more elaborate tests
    Signed-off-by: Patrik Oldsberg <patrik.oldsberg@ericsson.com>

---
+++ test/service_test.go
@@ -0,0 +1,160 @@
+// Copyright 2015 OpenMarket Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package events
+
+import (
+	"testing"
+
+	"github.com/Rugvip/bullettime/db"
+	"github.com/Rugvip/bullettime/events"
+	"github.com/Rugvip/bullettime/interfaces"
+	"github.com/Rugvip/bullettime/service"
+	"github.com/Rugvip/bullettime/types"
+)
+
+type services struct {
+	room     interfaces.RoomService
+	user     interfaces.UserService
+	profile  interfaces.ProfileService
+	presence interfaces.PresenceService
+	token    interfaces.TokenService
+	event    interfaces.EventService
+	sync     interfaces.SyncService
+}
+
+func setup() services {
+	roomStore, err := db.NewRoomDb()
+	if err != nil {
+		panic(err)
+	}
+	userStore, err := db.NewUserDb()
+	if err != nil {
+		panic(err)
+	}
+	aliasStore, err := db.NewAliasDb()
+	if err != nil {
+		panic(err)
+	}
+	memberStore, err := db.NewMembershipDb()
+	if err != nil {
+		panic(err)
+	}
+	streamMux, err := events.NewStreamMux()
+	if err != nil {
+		panic(err)
+	}
+	messageStream, err := events.NewMessageStream(memberStore, streamMux)
+	if err != nil {
+		panic(err)
+	}
+	presenceStream, err := events.NewPresenceStream(memberStore, streamMux)
+	if err != nil {
+		panic(err)
+	}
+	typingStream, err := events.NewTypingStream(memberStore, streamMux)
+	if err != nil {
+		panic(err)
+	}
+
+	roomService, err := service.CreateRoomService(
+		roomStore,
+		aliasStore,
+		memberStore,
+		messageStream,
+		presenceStream,
+		typingStream,
+		typingStream,
+	)
+	if err != nil {
+		panic(err)
+	}
+	userService, err := service.CreateUserService(userStore)
+	if err != nil {
+		panic(err)
+	}
+	profileService, err := service.NewProfileService(
+		presenceStream,
+		presenceStream,
+		memberStore,
+		roomStore,
+		messageStream,
+	)
+	if err != nil {
+		panic(err)
+	}
+	presenceService, err := service.NewPresenceService(presenceStream, presenceStream)
+	if err != nil {
+		panic(err)
+	}
+	tokenService, err := service.CreateTokenService()
+	if err != nil {
+		panic(err)
+	}
+	eventService, err := service.NewEventService(
+		messageStream,
+		presenceStream,
+		typingStream,
+		streamMux,
+		messageStream,
+		memberStore,
+	)
+	if err != nil {
+		panic(err)
+	}
+	syncService, err := service.NewSyncService(
+		messageStream,
+		presenceStream,
+		typingStream,
+		roomStore,
+		memberStore,
+	)
+	if err != nil {
+		panic(err)
+	}
+	return services{
+		roomService,
+		userService,
+		profileService,
+		presenceService,
+		tokenService,
+		eventService,
+		syncService,
+	}
+}
+
+func TestUserCreation(t *testing.T) {
+	s := setup()
+	userId := types.NewUserId("test", "matrix.org")
+	if err := s.user.CreateUser(userId); err != nil {
+		t.Fatal(err)
+	}
+	s.user.UserExists(userId, userId)
+	err := s.user.CreateUser(userId)
+	if err == nil {
+		t.Fatal("expected M_USER_IN_USE error")
+	} else if err.Code() != "M_USER_IN_USE" {
+		t.Error("expected M_USER_IN_USE error code but got ", err.Code())
+	}
+	status, err := s.presence.Status(userId, userId)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if status.Presence != types.PresenceOffline {
+		t.Error("expected offline presence")
+	}
+	if status.StatusMessage != "" {
+		t.Error("expected empty status message")
+	}
+}
